1c74f0f520ab196ab866cb01d642c72f88187f43
Ricardo <ricarditomontserrat@gmail.com>
2023-07-29 07:40:11 +0200

    Full regex test site, so that you can regex what you need in real time

---
+++ index.js
@@ -0,0 +1,36 @@
+const resultTextarea = document.getElementById("result-textarea");
+const urlInput = document.getElementById("url-input");
+const inputTextarea = document.getElementById("input-textarea");
+const regexInput = document.getElementById("regex");
+const showError = (error) => (resultTextarea.value = error);
+
+const defaultUrl = "https://listado.mercadolibre.com.ve/portaminas-delguard";
+const defaultRegex = 'class="ui-search-item__title shops__item-title">(.+?)</h2>';
+
+fetch("mercado.html").then(async (response) => {
+	urlInput.value = defaultUrl;
+	regexInput.value = defaultRegex;
+	inputTextarea.value = await response.text();
+	matchRegex(defaultRegex);
+});
+
+function matchRegex(regex) {
+	const input = inputTextarea.value;
+	const matches = input.match(regex);
+	resultTextarea.value = matches[0];
+	for (let i = 1; i < matches.length; i++) {
+		resultTextarea.value += `\t\t\t${matches[i]}`;
+	}
+}
+
+document.getElementById("fetch-button").addEventListener("click", () => {
+	resultTextarea.value = "";
+	fetch(urlInput.value)
+		.then(async (response) => (inputTextarea.value = await response.text()))
+		.catch(showError);
+});
+
+regexInput.addEventListener("input", (e) => {
+	const regex = e.target.value;
+	matchRegex(regex);
+});
