6bcbabc18d43518f96933cf43604bfe4ecdd86ff
Ricardo <ricarditomontserrat@gmail.com>
2023-09-02 10:07:18 +0200

    Drag and drop files, groups and results better overview, copy to clickboard button

--- index.js
+++ index.js
@@ -1,37 +1,79 @@
-const resultTextarea = document.getElementById("result-textarea");
-const urlInput = document.getElementById("url-input");
-const inputTextarea = document.getElementById("input-textarea");
-const regexInput = document.getElementById("regex");
-const showError = (error) => (resultTextarea.value = error);
-
-const defaultUrl = "https://listado.mercadolibre.com.ve/portaminas-delguard";
-const defaultRegex = 'class="ui-search-item__title shops__item-title">(.+?)</h2>';
-
-fetch("mercado.html").then(async (response) => {
-	urlInput.value = defaultUrl;
-	regexInput.value = defaultRegex;
-	inputTextarea.value = await response.text();
-	matchRegex(defaultRegex);
-});
+const textareaMatches = document.getElementById("matches-textarea");
+const textareaGroup = document.getElementById("groups-textarea");
+const textareaInput = document.getElementById("input-textarea");
+const inputUrl = document.getElementById("url-input");
+const inputRegex = document.getElementById("regex");
+const buttonFetch = document.getElementById("fetch-button");
+const showError = (error) => (textareaMatches.value = error);
 
 function matchRegex(regex) {
-	const input = inputTextarea.value;
-	const matches = input.match(regex);
-	if (!matches) return (resultTextarea.value = "no matches");
-	resultTextarea.value = matches[0];
-	for (let i = 1; i < matches.length; i++) {
-		resultTextarea.value += `\t\t\t${matches[i]}`;
+	const input = textareaInput.value;
+	const matches = input.matchAll(regex);
+
+	let next = matches.next();
+	textareaMatches.value = textareaGroup.value = next.done ? "no matches" : "";
+
+	while (!next.done) {
+		const match = next.value;
+
+		textareaMatches.value += match[0] + "\n";
+		textareaGroup.value += match[1] + "\n";
+
+		next = matches.next();
 	}
 }
 
-document.getElementById("fetch-button").addEventListener("click", () => {
-	resultTextarea.value = "";
-	fetch(urlInput.value)
-		.then(async (response) => (inputTextarea.value = await response.text()))
-		.catch(showError);
+for (const button of document.getElementsByClassName("copy-button")) {
+	const textarea = button.nextElementSibling;
+	button.addEventListener("click", () => {
+		navigator.clipboard.writeText(textarea.value);
+		button.classList.add("animate");
+		button.textContent = "Copied!";
+	});
+	button.addEventListener("animationend", () => {
+		button.classList.remove("animate");
+		button.textContent = "Copy";
+	});
+}
+
+document.addEventListener("dragover", (e) => {
+	e.preventDefault();
+	document.body.style.opacity = 0.5;
 });
 
-regexInput.addEventListener("input", (e) => {
+document.addEventListener("dragleave", (e) => {
+	e.preventDefault();
+	document.body.style.opacity = 1;
+});
+
+document.addEventListener("drop", (e) => {
+	e.preventDefault();
+	document.body.style.opacity = 1;
+	const file = e.dataTransfer.files[0];
+	const reader = new FileReader();
+	reader.addEventListener("load", () => {
+		textareaInput.value = reader.result;
+		inputUrl.value = file.name + " (Success)";
+		matchRegex(inputRegex.value);
+	});
+	inputUrl.value = file.name;
+	reader.readAsText(file);
+});
+
+buttonFetch.addEventListener("click", () => {
+	textareaMatches.value = "";
+	buttonFetch.disabled = true;
+	fetch(inputUrl.value)
+		.then(async (response) => {
+			console.log(response);
+			textareaInput.value = await response.text();
+			matchRegex(inputRegex.value);
+		})
+		.catch(showError)
+		.then(() => (buttonFetch.disabled = false));
+});
+
+inputRegex.addEventListener("input", (e) => {
 	const regex = e.target.value;
 	matchRegex(regex);
 });
